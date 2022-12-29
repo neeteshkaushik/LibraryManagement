49e645e710f9495dc6cddc9e9e4be0f13f63a7fd
neeteshkaushik <36589237+neeteshkaushik@users.noreply.github.com>
2022-12-29 12:05:21 +0530

    added cors support

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv");
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const libraryRoutes = require("./routes/main");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 app.use("/api/v1/library", libraryRoutes);
