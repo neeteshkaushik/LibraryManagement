3265315ca993812df62124e6328f0b5978c02d91
neeteshkaushik <36589237+neeteshkaushik@users.noreply.github.com>
2022-12-29 12:18:47 +0530

    added swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 const libraryRoutes = require("./routes/main");
 const app = express();
 
+//swagger implementation
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
@@ -13,6 +18,7 @@ app.use("/api/v1/library", libraryRoutes);
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
 });
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
